10a6fe4ce3ccda075cb67b36346e99a3892a128f
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 19:25:02 +0300

    out keyword ile calisildi

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Add();
+//Add();
 
-var result = Add2(10);
-Console.WriteLine(result);
+//var result = Add2(10);
+//Console.WriteLine(result);
 
-int number1 = 100;
+int number1 ;
 int number2 = 20;
-var result2= Add3(ref number1,number2);
+var result2= Add3(out number1,number2);
 Console.WriteLine(result2);
 Console.WriteLine(number1);
 
@@ -19,9 +19,9 @@ static int Add2 (int x ,int number1=10,int number2=20)
     var result = number1 + number2+ x;
     return result;
 }
-static int Add3 (ref int number1 ,int number2)
+static int Add3 (out int number1 ,int number2)
 {
-    number1 = 30;
+    number1 = 30; //out kullanırsak metot içinde set edilmiş olmalı 
     return number1 + number2;
 }
 Console.ReadLine();
