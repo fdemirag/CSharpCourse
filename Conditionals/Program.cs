4efd08b3b97886875c51dd8e65c31813747b68bd
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 16:13:36 +0300

    single line if ogrenildi

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var number = 10;
-if (number == 10)
+var number = 11;
+/* if (number == 10)
 {
     Console.WriteLine("number is 10");
 }
 else
 {
     Console.WriteLine("number is not 10");
-}
+} */
+
+Console.WriteLine(number == 10 ? "number is 10" : "number is not 10");
+
 Console.ReadLine();
 
