b19388843ed3d4f7e77b9d4d839b277500b874d1
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 15:33:58 +0300

    long veri tipi ogrenildi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//value types
+//Console.WriteLine("Hello, World!");
+int number1 = 2147483647;
+//long number2 = -9223372036854775808;
+long number2 = 9223372036854775807;
+Console.WriteLine("Number1 is {0}", number1);
+Console.WriteLine("Number1 is {0}", number2);
 Console.ReadLine();
