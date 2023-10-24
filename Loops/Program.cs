a8eea71b1db66e990ba6a537ece39cf783ecb6aa
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 20:24:03 +0300

    asal sayi uygulamasi yapildi

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -2,15 +2,44 @@
 {
     private static void Main(string[] args)
     {
+        if (IsPrimeNumber(6))
+        {
+            Console.WriteLine("this is a prime number");
+        }
+        else
+        {
+            Console.WriteLine("this is not a prime number");
+        }
+        
+
+
+
+        Console.ReadLine();
+    }
+
+    private static bool IsPrimeNumber(int number)
+    {
+        bool result = true;
+        for (int i = 2; i < number-1; i++)
+        {
+            if (number%i==0)
+            {
+                result = false;
+                i = number;
+            }
+        }
+        return result;
+    }
+
+
+    private static void ForeachLoop()
+    {
         string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
         foreach (var student in students)
         {
             Console.WriteLine(student);
         }
-
-
-        Console.ReadLine();
     }
 
     private static void DoWhileLoop()
