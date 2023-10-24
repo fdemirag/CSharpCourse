58016265902a6c2eb6faa172916841cec99f68a3
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 19:20:37 +0300

    ref keyword ile calisildi

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -3,6 +3,13 @@ Add();
 
 var result = Add2(10);
 Console.WriteLine(result);
+
+int number1 = 100;
+int number2 = 20;
+var result2= Add3(ref number1,number2);
+Console.WriteLine(result2);
+Console.WriteLine(number1);
+
 static void Add ()
 {
     Console.WriteLine("added!");
@@ -11,5 +18,10 @@ static int Add2 (int x ,int number1=10,int number2=20)
 {
     var result = number1 + number2+ x;
     return result;
+}
+static int Add3 (ref int number1 ,int number2)
+{
+    number1 = 30;
+    return number1 + number2;
 }
 Console.ReadLine();
