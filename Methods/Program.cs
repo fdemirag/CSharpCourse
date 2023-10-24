015e08f8d1f68e0a0928f2479a3e67fc8d550237
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 17:08:33 +0300

    parametreli metotlarla calisildi

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-add();
+Add();
 
-static void add ()
+var result = Add2(10,20);
+Console.WriteLine(result);
+static void Add ()
 {
     Console.WriteLine("added!");
+}
+static int Add2 (int number1,int number2)
+{
+    var result = number1 + number2;
+    return result;
 }
 Console.ReadLine();
