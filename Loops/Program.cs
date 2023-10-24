1447b5cb372874e28273853a704386d2815c9966
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 20:09:43 +0300

    while dongusu ile calisildi

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -2,10 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        for (int i = 100; i >=0; i=i-2)
+        int number = 100;
+        while (number >= 0)
+        {
+            Console.WriteLine(number);
+            number--;
+        }
+
+         Console.ReadLine();
+    }
+
+    private static void ForLoop()
+    {
+        for (int i = 100; i >= 0; i = i - 2)
         {
             Console.WriteLine(i);
-     
-        }       Console.ReadLine();
+
+        }
     }
 }
