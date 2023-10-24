367e64acaf87f325a73a7504293e9397783d3a60
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 20:05:35 +0300

    for dongusu ile calisildi

---
+++ Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 100; i >=0; i=i-2)
+        {
+            Console.WriteLine(i);
+     
+        }       Console.ReadLine();
+    }
+}
