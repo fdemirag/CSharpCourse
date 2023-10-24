90424aeb3a8b2a8f809231dcea8af5011f8ad3cd
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 19:51:02 +0300

    diziler ile calisildi

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //string[] students = new string[3];
+        //students[0] = "Engin";
+        //students[1] = "Derin";
+        //students[2] = "Salih";
+
+        string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+        string[] students2 = { "Engin", "Derin", "Salih" };
+
+        foreach (var student in students2)
+        {
+            Console.WriteLine(student);
+        }
+        Console.Read();
+    }
+}
