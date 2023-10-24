4cc0ed93ef4ef052572d7e20d19f957307d037c8
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 20:54:38 +0300

    String metotlariyla calisildi

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -2,7 +2,32 @@
 {
     private static void Main(string[] args)
     {
-        String city = "Ankara";
+
+        string sentence = "My name is Fatih Demirağ";
+        var result = sentence.Length;
+        var result2 = sentence.Clone();
+        sentence = "My name is Mr Demirağ";
+        var result3 = sentence.EndsWith("ğ");
+        var result4 = sentence.StartsWith("My name");
+        var result5 = sentence.IndexOf("namee");
+        var result6 = sentence.IndexOf("name");
+        var result7 = sentence.LastIndexOf(" ");
+        var result8 = sentence.Insert(0, "Hello, ");
+        var result9 = sentence.Substring(3, 4);
+        var result10 = sentence.ToLower();
+        var result11 = sentence.ToUpper();
+        var result12 = sentence.Replace(" ", "-");
+        var result13 = sentence.Remove(2, 5);
+        Console.WriteLine(result11);
+        Console.ReadLine();
+
+
+        Console.ReadLine();
+    }
+
+    private static void Intro()
+    {
+        string city = "Ankara";
         //Console.WriteLine(city[4]);
 
         foreach (var item in city)
@@ -12,9 +37,6 @@
 
         String city2 = "İstanbul";
 
-        Console.WriteLine(String.Format("{0} {1}" ,city,city2));
-
-        Console.ReadLine();
+        Console.WriteLine(String.Format("{0} {1}", city, city2));
     }
-    
 }
