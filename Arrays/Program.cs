38965a193151cee9a92bacf9fc738e8072a0d113
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 20:01:21 +0300

    Cok boyutlu diziler ile calisildi.

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,14 +7,33 @@
         //students[1] = "Derin";
         //students[2] = "Salih";
 
-        string[] students = new string[3] { "Engin", "Derin", "Salih" };
+        //string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
-        string[] students2 = { "Engin", "Derin", "Salih" };
+        //string[] students2 = { "Engin", "Derin", "Salih" };
 
-        foreach (var student in students2)
+        //foreach (var student in students2)
+        //{
+        //    Console.WriteLine(student);
+        //}
+
+        string[,] region = new string[5, 3]
         {
-            Console.WriteLine(student);
+            {"İstanbul","İzmit","Balıkesir"},
+            {"Ankara","Konya","Kırıkkale"},
+            {"Antalya","Adana","Mersin"},
+            {"Rize","Trabzon","Samsun"},
+            {"İzmir","Muğla","Manisa"}
+        };
+        for (int i = 0; i <= region.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= region.GetUpperBound(1); j++)
+            {
+                Console.WriteLine(region[i,j]);
+            }
         }
+
+
         Console.Read();
+
     }
 }
