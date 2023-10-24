5eee831ae3d1d891861acfc4786556617d5f87de
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 19:44:18 +0300

    params keyword ile calisildi

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -29,21 +29,24 @@ internal class Program
         //Console.WriteLine(result2);
         //Console.WriteLine(number1);
 
-        Console.WriteLine(Multiply(2,3));
-        Console.WriteLine(Multiply(2,3,4));
+        //Console.WriteLine(Multiply(2, 3));
+        //Console.WriteLine(Multiply(2, 3, 4));
 
 
-
+        Console.WriteLine(Add4(4,5,5,4,3,3,3,2,1));
 
         Console.ReadLine();
 
     }
-    static int Multiply(int number1, int number2)
-        {
-            return number1 * number2;
-        }
-        static int Multiply(int number1, int number2, int number3)
-        {
-            return number1 * number2 * number3;
+    //static int Multiply(int number1, int number2)
+    //{
+    //    return number1 * number2;
+    //}
+    //static int Multiply(int number1, int number2, int number3)
+    //{
+    //    return number1 * number2 * number3;
+    //}
+    static int Add4 (int number,params int[] numbers){
+        return numbers.Sum();
         }
 }
