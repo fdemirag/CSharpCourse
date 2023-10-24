58489a0fd36de50a1914282765d599fca9c56b34
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 15:40:56 +0300

    byte veri tipi öğrenildi.

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //value types
 //Console.WriteLine("Hello, World!");
+byte number4 = 255;
+
 //short number3 = 32767;
 short number3 = -32768;
 
@@ -10,4 +12,5 @@ long number2 = 9223372036854775807;
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number1 is {0}", number2);
 Console.WriteLine("Number1 is {0}", number3);
+Console.WriteLine("Number1 is {0}", number4);
 Console.ReadLine();
