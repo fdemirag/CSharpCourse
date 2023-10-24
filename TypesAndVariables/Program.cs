e522d29bc06e3106d0de6664df2408a9b6fa6bc6
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 15:36:06 +0300

    short veri tipi ogrenildi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //value types
 //Console.WriteLine("Hello, World!");
+//short number3 = 32767;
+short number3 = -32768;
+
 int number1 = 2147483647;
 //long number2 = -9223372036854775808;
 long number2 = 9223372036854775807;
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number1 is {0}", number2);
+Console.WriteLine("Number1 is {0}", number3);
 Console.ReadLine();
