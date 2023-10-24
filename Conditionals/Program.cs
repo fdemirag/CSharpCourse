c98e513fc901dfad4c3703d92b05de3d57faba33
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 16:09:25 +0300

    if else bloklari ogrenildi.

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var number = 10;
+if (number == 10)
+{
+    Console.WriteLine("number is 10");
+}
+else
+{
+    Console.WriteLine("number is not 10");
+}
+Console.ReadLine();
 
