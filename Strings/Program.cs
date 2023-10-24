74288e95358ae8e9aa1bba05f55c3cfa9f83b509
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 20:33:29 +0300

    Stringler ile calismaya baslamak

---
+++ Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String city = "Ankara";
+        //Console.WriteLine(city[4]);
+
+        foreach (var item in city)
+        {
+            Console.WriteLine(item);
+        }
+
+        String city2 = "İstanbul";
+
+        Console.WriteLine(String.Format("{0} {1}" ,city,city2));
+
+        Console.ReadLine();
+    }
+    
+}
