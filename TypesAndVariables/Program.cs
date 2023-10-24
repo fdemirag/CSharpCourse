95b43844e829d5b234c317f30835e5e1a0156ef6
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-24 15:48:18 +0300

    double veri tipi ogrenildi.

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -5,6 +5,7 @@ bool condition = true;
 
 //bool condition = false;
 char character = 'A';
+double number5 = 10.4;
 
 
 byte number4 = 255;
@@ -16,9 +17,10 @@ int number1 = 2147483647;
 //long number2 = -9223372036854775808;
 long number2 = 9223372036854775807;
 Console.WriteLine("Number1 is {0}", number1);
-Console.WriteLine("Number1 is {0}", number2);
-Console.WriteLine("Number1 is {0}", number3);
-Console.WriteLine("Number1 is {0}", number4);
+Console.WriteLine("Number2 is {0}", number2);
+Console.WriteLine("Number3 is {0}", number3);
+Console.WriteLine("Number4 is {0}", number4);
+Console.WriteLine("Number5 is {0}", number5);
 Console.WriteLine("Character is {0};", character);
 
 Console.WriteLine("Character is {0};", (int)character);
