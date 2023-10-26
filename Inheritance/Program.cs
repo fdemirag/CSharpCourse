53a23c93b1d6e3f718b270a9907a7a9adba7c208
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-26 19:20:58 +0300

    Inheritance calisildi

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName="Fatih"
+                },
+                new Student
+                {
+                    FirstName="Mehmet"
+                },
+                new Person
+                {
+                    FirstName="asd"
+                }
+            };
+        foreach (var person in persons)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set;}
+    }
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+    class Student:Person
+    {
+        public string Department { get; set; }
+    }
+}
+    
