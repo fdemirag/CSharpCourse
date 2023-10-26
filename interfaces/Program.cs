888df3a5acdb92492fc66ffcc4d1b3d6f3394515
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-26 17:46:46 +0300

    interface calismasi yapildi

--- interfaces/Program.cs
+++ interfaces/Program.cs
@@ -2,30 +2,67 @@
 {
     private static void Main(string[] args)
     {
+        PersonManager manager = new PersonManager();
+        Customer customer = new Customer
+        {
+            Id = 1,
+            FirstName = "Fatih",
+            SecondName = "Demirağ",
+            Adress = "Bursa"
+        };
+        Student student = new Student
+        {
+            Id = 2,
+            FirstName = "Fatih",
+            SecondName = "Demirağ",
+            Deparmant = "Yazilimci Adayi"
+        };
+        manager.add(customer);
+        manager.add(student);
 
+        Console.ReadLine();
+                
+                    
     }
     interface IPerson
     {
         int Id { get; set; }
-        int FirstName { get; set; }
-        int SecondName { get; set; }
+        string FirstName { get; set; }
+        string SecondName { get; set; }
 
     }
     class Customer : IPerson
     {
-        public int Id { get ; set ; }
-        public int FirstName { get; set; }
-        public int SecondName { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
 
         public string Adress { get; set; }
     }
-    class Student:IPerson
+    class Student : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int SecondName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
 
 
         public string Deparmant { get; set; }
+    }
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+
+        public string Deparmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void add(IPerson person)
+        {
+            Console.WriteLine(person.SecondName);
+        }
     }
 }
