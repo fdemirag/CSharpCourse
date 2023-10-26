c7da4803a20dcf33431ce318424e045c771311f0
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-26 14:18:52 +0300

    Encapsulation calisildi

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -3,11 +3,25 @@ namespace Classes
 {
 	public class Customer
 	{
+		//field
 
-		public string FirstName;
+		//public string FirstName;
+
+		//property
+
 		public int Id { get; set; }
-		//public string FirstName{get; set;}
-		public string LastName { get; set; }
+		public string FirstName{get; set;}
+
+
+        /* private string _FirstName; 
+         *  public string FirstName{
+         *  get{ return "Mr."+_FirstName;} 
+         *  set{_FirstName = value;}
+         *  }
+
+
+         */
+        public string LastName { get; set; }
 		public string City { get; set; }
 	}
 }
