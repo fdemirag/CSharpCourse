6e043f2edf56be5e07e9f495fae0d2dc80852187
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-26 13:44:31 +0300

    Customer Class ı olusturuldu

---
+++ Classes/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Classes
+{
+	public class Customer
+	{
+
+		public string FirstName;
+		public int Id { get; set; }
+		//public string FirstName{get; set;}
+		public string LastName { get; set; }
+		public string City { get; set; }
+	}
+}
+
