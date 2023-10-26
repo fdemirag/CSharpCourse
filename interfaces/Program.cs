ce74e058e737274ac12a71491d423c38454eb31a
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-10-26 17:31:15 +0300

    interface giris yapildi

---
+++ interfaces/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        int FirstName { get; set; }
+        int SecondName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get ; set ; }
+        public int FirstName { get; set; }
+        public int SecondName { get; set; }
+
+        public string Adress { get; set; }
+    }
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int SecondName { get; set; }
+
+
+        public string Deparmant { get; set; }
+    }
+}
